ed967ebe1865f26a2aecbae12725442337c402f2
Dai Dong <dong.dai@ttu.edu>
2016-07-28 16:50:59 -0500

    CloudLab scripts

--- scripts/cloudlab/SetupCluster.sh
+++ scripts/cloudlab/SetupCluster.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
 
 # This script should run in head node
+while [ -h "$PRG" ] ; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`/"$link"
+  fi
+done
+
+PRGDIR=`dirname "$PRG"`
+[ -z "$PROCESSOR_HOME" ] && PROCESSOR_HOME=`cd "$PRGDIR" ; pwd`
 
 # Setup Head Node
 cp /proj/cloudincr-PG0/tools/installers/id_rsa ~/.ssh/
 cp /proj/cloudincr-PG0/tools/installers/id_dsa ~/.ssh/
 ssh-agent bash
 ssh-add
+exit
 
 bound=`expr $1 - 1`
 
@@ -19,5 +32,5 @@ done
 # Install Packages
 for i in $(seq 0 $bound)
 do
-	ssh -t node-$i "./package-installer.sh" &
+	ssh -t node-$i "$PROCESSOR_HOME/package-installer.sh" &
 done
