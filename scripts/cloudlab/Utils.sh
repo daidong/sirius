02c9863a3e81d9df601e8fb7d73f5bbfe7789fee
Dai Dong <dong.dai@ttu.edu>
2016-07-28 18:02:13 -0500

    cloudlab script

--- scripts/cloudlab/Utils.sh
+++ scripts/cloudlab/Utils.sh
@@ -26,7 +26,7 @@ if [ "UPDATE" = $2 ]; then
 	for i in $(seq 0 $bound)
 	do
     	echo UPDATE Node-$i
-    	ssh node-$i "cd ~/iogp-code; git pull; make all"
+    	ssh node-$i "cd ~/iogp-code; git pull; make all" &
 	done
 fi
 
