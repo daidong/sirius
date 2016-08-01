a804f3421f01ca49dbe69863259a163b8babcd3f
Dai Dong <dong.dai@ttu.edu>
2016-08-01 02:30:59 -0500

    remove fennel info outputs

--- java/src/main/java/edu/ttu/discl/iogp/utils/Constants.java
+++ java/src/main/java/edu/ttu/discl/iogp/utils/Constants.java
@@ -15,7 +15,7 @@ public class Constants {
 	public final static int EDGE_SPLIT_WRONG_SRV = 2;
 
 	public final static int SPLIT_THRESHOLD = 60000;
-	public final static int REASSIGN_THRESHOLD = 6;
+	public final static int REASSIGN_THRESHOLD = 60;
 
 	public static int RETRY = 3;
 	
