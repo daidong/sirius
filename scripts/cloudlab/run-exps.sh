c68d3888a4b9bc0ddb1979c24d170a4bd0c59a68
Dai <daidongly@gmail.com>
2018-10-08 01:13:12 -0400

    update

---
+++ scripts/cloudlab/run-exps.sh
@@ -0,0 +1,13 @@
+/users/dirruncc/simplegdb-Java/scripts/cloudlab/Utils.sh 3 JPS
+
+/users/dirruncc/simplegdb-Java/scripts/cloudlab/Utils.sh 3 UPDATE
+
+/users/dirruncc/simplegdb-Java/scripts/cloudlab/Utils.sh 3 UPDATE-COMPILE
+
+/users/dirruncc/simplegdb-Java/scripts/cloudlab/Utils.sh 3 CLEAN
+
+/users/dirruncc/simplegdb-Java/scripts/cloudlab/StartServer.sh -n 3 -t edgecut -d ~/dbs/
+
+/users/dirruncc/simplegdb-Java/scripts/cloudlab/StopServer.sh -n 3
+
+~/simplegdb-Java/scripts/cloudlab/ClientOps.sh -n 3 -t edgecut -o travel -c 0 -g /proj/dirr-PG0/datasets/p2p-Gnutella04.txt
