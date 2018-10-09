d5f737139fa04fe82297fc69e3689e646eaf8ff0
Dai <daidongly@gmail.com>
2018-10-09 11:57:28 -0400

    update setup script

--- scripts/cloudlab/SetupCluster.sh
+++ scripts/cloudlab/SetupCluster.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/env bash
 
-# This script should run in head node
-while [ -h "$PRG" ] ; do
-  ls=`ls -ld "$PRG"`
-  link=`expr "$ls" : '.*-> \(.*\)$'`
-  if expr "$link" : '/.*' > /dev/null; then
-    PRG="$link"
-  else
-    PRG=`dirname "$PRG"`/"$link"
-  fi
-done
-
-PRGDIR=`dirname "$PRG"`
-[ -z "$PROCESSOR_HOME" ] && PROCESSOR_HOME=`cd "$PRGDIR" ; pwd`
-
 bound=`expr $1 - 1`
 
 # Touch Every Server
@@ -25,7 +11,5 @@ done
 # Install Packages
 for i in $(seq 0 $bound)
 do
-	ssh -t node$i "$PROCESSOR_HOME/package-installer.sh" &
+	ssh -t node$i "~/simplegdb-Java/scripts/cloudlab//package-installer.sh" &
 done
-
-echo "$PROCESSOR_HOME"
