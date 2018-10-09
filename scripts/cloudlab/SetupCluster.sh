cff96edd4e772275a090ddf2c36bb246844783cc
Dai <daidongly@gmail.com>
2018-10-09 11:58:46 -0400

    update setup script

--- scripts/cloudlab/SetupCluster.sh
+++ scripts/cloudlab/SetupCluster.sh
@@ -11,5 +11,6 @@ done
 # Install Packages
 for i in $(seq 0 $bound)
 do
+  ssh -t node$i "cd ~; git clone https://github.com/daidong/simplegdb-Java.git"
 	ssh -t node$i "~/simplegdb-Java/scripts/cloudlab//package-installer.sh" &
 done
